a2af4412ac6ea487f8991022799d068b1a4be684
fratyaldiz <firatylz2004@hotmail.com>
2026-01-03 15:39:28 +0300

    init comit

--- src/Tree1/BinaryTree.java
+++ src/Tree1/BinaryTree.java
@@ -56,5 +56,12 @@ public class BinaryTree {
         return Math.max(tree_height(root.left), tree_height(root.right)) + 1;
     }
 
+    int size(Node root) {
+        if (root == null) return 0;
+        else {
+            return 1 + size(root.left) + size(root.right);
+        }
+    }
+
 
 }
